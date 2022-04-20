801b46d751c6372bcd8c8eefeb20962846ecbea6
chocopipie <78339579+chocopipie@users.noreply.github.com>
2022-04-19 23:38:44 -0700

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,11 +6,11 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
     Database database;
-    //database.populateColleges("../CollegeConnect/Distances.csv");
-    //database.populateSouvenirs("../CollegeConnect/Souvenirs.csv");
+    database.populateColleges("../CollegeConnect/Distances.csv");
+    database.populateSouvenirs("../CollegeConnect/Souvenirs.csv");
 
-    database.populateColleges("C:/csv/Distances.csv");
-    database.populateSouvenirs("C:/csv/Souvenirs.csv");
+    //database.populateColleges("C:/csv/Distances.csv");
+    //database.populateSouvenirs("C:/csv/Souvenirs.csv");
 
 }
 
