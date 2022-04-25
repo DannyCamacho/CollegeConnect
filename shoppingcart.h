ea2478daeec38b5f9623052e9dbaef70480a91e1
Daniel Camacho <dcamacho4@saddleback.edu>
2022-04-25 16:45:26 -0700

    Coding adjustments.

--- shoppingcart.h
+++ shoppingcart.h
@@ -3,13 +3,10 @@
 
 #include <QMainWindow>
 
-namespace Ui {
-class ShoppingCart;
-}
-
-class ShoppingCart : public QMainWindow
-{
+namespace Ui { class ShoppingCart; }
+class ShoppingCart : public QMainWindow {
     Q_OBJECT
+
 public:
     explicit ShoppingCart(QWidget *parent = nullptr);
     ~ShoppingCart();
